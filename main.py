9c7f3fad41a2dfbfad9db50c04dfdeaa7723ad8b
Tuyencoder <tuyenmimo@gmail.com>
2025-12-20 13:54:13 +0700

    Them do uong vao menu

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello cac ban")
+def show_menu():
+    print("Hello cac ban")
+    dict = {
+        "Nước ngọt": 15000,
+        "Trà sữa": 25000,
+        "Cà phê": 20000
+    }
+    print("Máy bán hàng tự động của nhóm 36: [{Menu}:{1. Nước ngọt, Trà sữa, Cà phê}]")
+    if __name__ == "__main__":
+        show_menu()
